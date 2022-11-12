07e661e8ddb8c447b257667e3647a2e10f667fcc
Kulwinder Rupal <kulwinder_rupal@yahoo.com>
2022-11-12 16:19:08 +1100

    Implementing sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 import {
     Container,
     SemiBold,
@@ -7,17 +7,25 @@ import {
     StyledLabel,
     StyledParagraph,
 } from './common/styles';
+import { IHotel } from './common/types';
 import { SearchRecord } from './components/SearchRecord';
 import logo from './images/qantas-logo.png';
-import { fetch, hotels } from './slices/results.slice';
 
 function App() {
-    const dispatch = useDispatch();
-    const results = useSelector(hotels);
+    const [results, setResults] = useState<IHotel[]>([]);
+    const getResults = async () =>
+        await Axios({
+            url: 'http://localhost:3001/results',
+            method: 'GET',
+        });
     useEffect(() => {
-        dispatch(fetch());
+        getResults().then((resp) => {
+            const sortedResults = sortByPrice('l2h', resp.data);
+            setResults(sortedResults);
+        });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
     return (
         <Container>
             <img src={logo} alt="qantas logo" width={160} />
@@ -32,9 +40,19 @@ function App() {
                             <StyledLabel htmlFor={'sortByPrice'}>
                                 Sort by &nbsp;
                             </StyledLabel>
-                            <select defaultValue={'l2h'} id={'sortByPrice'}>
-                                <option value={'l2h'}>Price high-low</option>
-                                <option value={'h2l'}>Price low-high</option>
+                            <select
+                                defaultValue={'l2h'}
+                                id={'sortByPrice'}
+                                onChange={(event) => {
+                                    const sortedResults = sortByPrice(
+                                        event.target.value,
+                                        results
+                                    );
+                                    setResults(sortedResults);
+                                }}
+                            >
+                                <option value={'l2h'}>Price low-high</option>
+                                <option value={'h2l'}>Price high-low</option>
                             </select>
                         </div>
                     </SortWrapper>
@@ -46,5 +64,15 @@ function App() {
         </Container>
     );
 }
+
+const sortByPrice = (sortOrder: string, results: IHotel[]) => {
+    return results.sort((hotelA, hotelB) => {
+        const priceHotelA = hotelA.offer.displayPrice.amount;
+        const priceHotelB = hotelB.offer.displayPrice.amount;
+        return sortOrder === 'l2h'
+            ? priceHotelA - priceHotelB
+            : priceHotelB - priceHotelA;
+    });
+};
 
 export default App;
